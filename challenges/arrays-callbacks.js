ad29207067479e83cc6abb7808a4dbbe43300426
sebastian espeset <sebastian.c.espeset@gmail.com>
2020-10-16 15:18:43 -0700

    FINALLY FINISHED

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -68,10 +68,13 @@ const lowPopulationAnimals=zooAnimals.filter(function(element){
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
-console.log(populationTotal);
+const populationCounter=0;
+const totalPopulation=zooAnimals.reduce(function(populationCounter,currentValue){
+     return currentValue.population+populationCounter;
+  // console.log(populationCounter);
+},0)
 
-
+console.log(totalPopulation);
 // ==== Callbacks ====  
 
 /* Step 1: Create a higher-order function
